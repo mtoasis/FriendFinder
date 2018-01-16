c6ffb8f2cde17d258a776aea3957a87be3630b7a
unknown <mtoasis22@gmail.com>
2018-01-15 22:04:01 -0800

    fin

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,30 +10,39 @@ module.exports = function(app) {
       res.json(friends)
   })
 
-  app.post("/api/friends", function(req, res) {
+  app.post("/api/friends", function(req, res) {    
     
-    var newScores = req.body.scores.split(" ");
-    
+    var newScores = req.body.scores.split("");    
 
     var newFriend = {
       name: req.body.name,
       photo: req.body.photo,
       scores: newScores
     }
-    friends.push(newFriend)
-    // console.log(friends[0].scores[0])
-    console.log()
-    // difference check
+
     var diff =[];
     for (var i=0, n=friends.length; i<n; i++){
-      // console.log(friends[i].scores)
+      var sum = 0;
       for (var j=0; j<10; j++){
-        // console.log(newScores[j])
-      // diff[i] += Math.abs(Number(newScores[j])-Number(friends[i].scores[j])) 
-      }
-    }
-    console.log(diff)
 
+        var c= Math.abs(Number(newScores[j])-Number(friends[i].scores[j]))
+        sum = sum+c
+        
+      }
+      diff[i] = Number(sum);
+    }
+
+    var minValue = Math.min.apply(null,diff);
+    var matching = [];
+    for (var i=0, n=diff.length; i<n; i++){
+      if(diff[i] == minValue){
+        matching.push(friends[i])
+      }      
+    }
+    
+    res.json(matching)
+
+    friends.push(newFriend)
   });
 
 };
